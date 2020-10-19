0a142f988193fc5cb1bdf314d62c13d0a9395059
danjuma <hda0202@gmail.com>
2020-10-19 21:53:39 +0100

    new commit

--- myblog_project/settings.py
+++ myblog_project/settings.py
@@ -131,3 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
